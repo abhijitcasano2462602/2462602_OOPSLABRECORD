a2f904823d732ff052ddc84f8214eaa9205ade23
abhijitcasano2462602 <abhijit.kumar@btech.christuniversity.in>
2026-02-26 09:48:26 +0530

    Add files via upload

---
+++ Exercise1_Basics/Exp_2.java
@@ -0,0 +1,20 @@
+package practical;
+public class Exp_2{
+		public static void main(String[] args) {
+			// primitives
+			System.out.println("REG NO : 2462602");
+			int a =10;
+			double b = 3.0;
+			char letter ='A';
+			boolean isJavaFun = true;
+				int sum =a+(int)b;
+			double division = a / b;
+			int remainder = a%3;
+			String msg ="Value of a :" + a +",b:" +b;
+			System.out.println(msg);
+			System.out.println("Sum ="+sum);
+			System.out.println("division ="+division);
+			System.out.println("remainder="+remainder);
+			System.out.println("letter= "+letter+",isJavaFun="+isJavaFun);
+		}
+}
